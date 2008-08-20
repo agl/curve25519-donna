fef0c248f4a34fe6d84f64e634dab6842a3aef3a
Adam Langley <agl@imperialviolet.org>
2008-08-20 10:46:56 -0700

    Add test suite

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-targets: curve25519-donna.a curve25519-donna-x86-64.a
+targets: curve25519-donna.a curve25519-donna-x86-64.a test-donna test-donna-x86-64
 
 clean:
-	rm *.o *.a *.pp
+	rm -f *.o *.a *.pp test-curve255-donna test-curve25519-donna-x86-64
 
 curve25519-donna.a: curve25519-donna.o
 	ar -rc curve25519-donna.a curve25519-donna.o
@@ -20,3 +20,17 @@ curve25519-donna-x86-64.s.pp: curve25519-donna-x86-64.s
 
 curve25519-donna-x86-64.o: curve25519-donna-x86-64.c
 	gcc -O2 -c curve25519-donna-x86-64.c
+
+test-donna: test-curve25519-donna
+	echo "Running test for C version"
+	./test-curve25519-donna | head -123456 | tail -1
+
+test-donna-x86-64: test-curve25519-donna-x86-64
+	echo "Running test for x86-64 version"
+	./test-curve25519-donna-x86-64 | head -123456 | tail -1
+
+test-curve25519-donna: test-curve25519.c curve25519-donna.a
+	gcc -o test-curve25519-donna test-curve25519.c curve25519-donna.a
+
+test-curve25519-donna-x86-64: test-curve25519.c curve25519-donna-x86-64.a
+	gcc -o test-curve25519-donna-x86-64 test-curve25519.c curve25519-donna-x86-64.a
---
+++ test-curve25519.c
@@ -0,0 +1,49 @@
+/*
+test-curve25519 version 20050915
+D. J. Bernstein
+Public domain.
+
+Tiny modifications by agl
+*/
+
+#include <stdio.h>
+
+extern void curve25519_donna(unsigned char *output, const unsigned char *a,
+                             const unsigned char *b);
+
+void doit(unsigned char *ek,unsigned char *e,unsigned char *k)
+{
+  int i;
+
+  for (i = 0;i < 32;++i) printf("%02x",(unsigned int) e[i]); printf(" ");
+  for (i = 0;i < 32;++i) printf("%02x",(unsigned int) k[i]); printf(" ");
+  curve25519_donna(ek,e,k);
+  for (i = 0;i < 32;++i) printf("%02x",(unsigned int) ek[i]); printf("\n");
+}
+
+unsigned char e1k[32];
+unsigned char e2k[32];
+unsigned char e1e2k[32];
+unsigned char e2e1k[32];
+unsigned char e1[32] = {3};
+unsigned char e2[32] = {5};
+unsigned char k[32] = {9};
+
+main()
+{
+  int loop;
+  int i;
+
+  for (loop = 0;loop < 1000000000;++loop) {
+    doit(e1k,e1,k);
+    doit(e2e1k,e2,e1k);
+    doit(e2k,e2,k);
+    doit(e1e2k,e1,e2k);
+    for (i = 0;i < 32;++i) if (e1e2k[i] != e2e1k[i]) printf("fail\n");
+    for (i = 0;i < 32;++i) e1[i] ^= e2k[i];
+    for (i = 0;i < 32;++i) e2[i] ^= e1k[i];
+    for (i = 0;i < 32;++i) k[i] ^= e1e2k[i];
+  }
+
+  return 0;
+}
